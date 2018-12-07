1ec29ed9ce92f3074488386f66913eee3b2ffdb3
bntnam <bntnam.app@gmail.com>
2018-12-08 00:32:43 +0100

    update

--- real_estate_app/settings.py
+++ real_estate_app/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'real_estate_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
