8f93fe9a552ed0c6f23609f846c203f82ce1fc62
bntnam <bntnam.app@gmail.com>
2018-12-10 22:22:02 +0100

    update

--- real_estate_connector/wsgi.py
+++ real_estate_connector/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for real_estate_app project.
+WSGI config for real_estate_connector project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real_estate_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real_estate_connector.settings')
 
 application = get_wsgi_application()
